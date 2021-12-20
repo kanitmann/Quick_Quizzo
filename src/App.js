a2025700603ff39e7e367b2e4697ce9c2e1e380e
kanitmann <kanitmann90@gmail.com>
2021-12-20 21:53:37 +0530

    Quiz App Completed :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,77 +1,82 @@
-import logo from './logo.svg';
-import React, { userState, useState } from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  const Questions = [
-    {
-      questionText: 'Qn1',
-      answerOptions: [
-        { answerText: "Text1", isTrue: true },
-        { answerText: "Text2", isTrue: false },
-        { answerText: "Text3", isTrue: false },
-        { answerText: "Text4", isTrue: false },
-      ]
-    },
-    {
-      questionText: 'Qn2',
-      answerOptions: [
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: true },
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: false },
-      ]
-    },
-    {
-      questionText: 'Qn3',
-      answerOptions: [
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: true },
-        { answerText: "", isTrue: false },
-      ]
-    },
-    {
-      questionText: 'Qn4',
-      answerOptions: [
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: false },
-        { answerText: "", isTrue: true },
-      ]
-    }
-  ];
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
 
-const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
-const clickHandler = ()=>{
-  const nextQuestion = currentQuestion + 1;
-  setCurrentQuestion(nextQuestion);
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-  return (
-
-    <div className='app'>
-      {
-        false ? (
-          <div className='scoreCard'>You scored 0 out of {Questions.length}</div>
-        ) : (
-          <>
-            <div className='score-section'>
-              <div className='question-number'>
-                <span>Question 1</span>/{Questions.length}
-              </div>
-              <div clasaName="question-text">{Questions[currentQuestion].questionText}
-              </div>
-            </div>
-            <div className='answer-section'>
-              {Questions[currentQuestion].answerOptions.map((answerOptions) => <button onClick={clickHandler}>{answerOptions.answerText}</button>)}
-            </div>
-          </>
-        )
-      }
-    </div>
-
-  );
-}
-
-export default App;
